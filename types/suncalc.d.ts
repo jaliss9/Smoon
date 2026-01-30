5dddb9d2cb70df2bcbb53fa38f9a0921ccc3fd7b
Jaliss Ch <jaliss@mac.mynetworksettings.com>
2026-01-30 17:39:18 -0500

    Add SunCalc type declarations

---
+++ types/suncalc.d.ts
@@ -0,0 +1,25 @@
+declare module 'suncalc' {
+  export interface MoonIllumination {
+    fraction: number;
+    phase: number;
+    angle: number;
+  }
+
+  export interface MoonPosition {
+    azimuth: number;
+    altitude: number;
+    distance: number;
+    parallacticAngle: number;
+  }
+
+  export interface MoonTimes {
+    rise?: Date;
+    set?: Date;
+    alwaysUp?: boolean;
+    alwaysDown?: boolean;
+  }
+
+  export function getMoonIllumination(date: Date): MoonIllumination;
+  export function getMoonPosition(date: Date, lat: number, lng: number): MoonPosition;
+  export function getMoonTimes(date: Date, lat: number, lng: number): MoonTimes;
+}
